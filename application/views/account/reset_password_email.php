bc5f96c7dd272643d1b4a09c4302462f7a6d1e65
Iraklis Mathiopoulos <mathiopoulos@gmail.com>
2013-02-27 15:40:58 +0200

    Added valid HTML formatted email.

--- application/views/account/reset_password_email.php
+++ application/views/account/reset_password_email.php
@@ -1,1 +1,10 @@
-<?php echo sprintf(lang('reset_password_email'), $username, $password_reset_url); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title></title>
+</head>
+<body>
+    <p><?php echo sprintf(lang('reset_password_email'), $username, $password_reset_url); ?></p>
+</body>
+</html>
