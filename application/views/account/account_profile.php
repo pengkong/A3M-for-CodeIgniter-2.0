1234a5aad20851a112e5fdd7270a33c377bc10d7
AdwinTrave <storyteller@freedombase.net>
2013-10-13 19:01:14 -0400

    Adding Gravatar title into language file
    For consistency

--- application/views/account/account_profile.php
+++ application/views/account/account_profile.php
@@ -15,37 +15,35 @@
         </div>
         <div class="span10">
 
-			<?php if (isset($profile_info))
+	<?php if (isset($profile_info))
 		{
-			?>
+	?>
             <div class="alert alert-success fade in">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
-				<?php echo $profile_info; ?>
+			<?php echo $profile_info; ?>
             </div>
-			<?php } ?>
+	<?php } ?>
 
             <h2><?php echo lang('profile_page_name'); ?></h2>
 
             <div class="well"><?php echo lang('profile_instructions'); ?></div>
 
-			<?php echo form_open_multipart(uri_string(), 'class="form-horizontal"'); ?>
-			<?php echo form_fieldset(); ?>
+		<?php echo form_open_multipart(uri_string(), 'class="form-horizontal"'); ?>
+		<?php echo form_fieldset(); ?>
 
             <div class="control-group <?php echo (form_error('profile_username')) ? 'error' : ''; ?>">
                 <label class="control-label" for="profile_username"><?php echo lang('profile_username'); ?></label>
 
                 <div class="controls">
-					<?php echo form_input(array('name' => 'profile_username', 'id' => 'profile_username', 'value' => set_value('profile_username') ? set_value('profile_username') : (isset($account->username) ? $account->username : ''), 'maxlength' => '24')); ?>
-					<?php if (form_error('profile_username') || isset($profile_username_error))
-				{
-					?>
+			<?php echo form_input(array('name' => 'profile_username', 'id' => 'profile_username', 'value' => set_value('profile_username') ? set_value('profile_username') : (isset($account->username) ? $account->username : ''), 'maxlength' => '24')); ?>
+			<?php if (form_error('profile_username') || isset($profile_username_error))
+			{
+			?>
                     <span class="help-inline">
-					<?php
-						echo form_error('profile_username');
-						echo isset($profile_username_error) ? $profile_username_error : '';
-						?>
-					</span>
-					<?php } ?>
+			<?php
+				echo form_error('profile_username');
+				echo isset($profile_username_error) ? $profile_username_error : '';} ?>
+			</span>
                 </div>
             </div>
 
@@ -54,58 +52,53 @@
 
                 <div class="controls">
                 <p>
-					<?php if (isset($account_details->picture) && strlen(trim($account_details->picture)) > 0) : ?>
-					<?php echo showPhoto($account_details->picture); ?> &nbsp;
-					<?php echo anchor('account/account_profile/index/delete', '<i class="icon-trash"></i> '.lang('profile_delete_picture'), 'class="btn"'); ?>
-					<?php else : ?>
-						
-						<div class="accountPicSelect clearfix">
-							<div class="pull-left">
-								<input type="radio" name="pic_selection" value="custom" checked="true" />
-								<?php echo showPhoto(); ?>
-							</div>
-							<div class="pull-left">
-								<p><?php echo lang('profile_custom_upload_picture'); ?><br>
-									<?php echo form_upload(array('name' => 'account_picture_upload', 'id' => 'account_picture_upload')); ?><br>
-									<small>(<?php echo lang('profile_picture_guidelines'); ?>)</small>
-								</p>
-							</div>
-						</div>
+			<?php if (isset($account_details->picture) && strlen(trim($account_details->picture)) > 0) : ?>
+			<?php echo showPhoto($account_details->picture); ?> &nbsp;
+			<?php echo anchor('account/account_profile/index/delete', '<i class="icon-trash"></i> '.lang('profile_delete_picture'), 'class="btn"'); ?>
+			<?php else : ?>
+				
+				<div class="accountPicSelect clearfix">
+					<div class="pull-left">
+						<input type="radio" name="pic_selection" value="custom" checked="true" />
+						<?php echo showPhoto(); ?>
+					</div>
+					<div class="pull-left">
+						<p><?php echo lang('profile_custom_upload_picture'); ?><br>
+							<?php echo form_upload(array('name' => 'account_picture_upload', 'id' => 'account_picture_upload')); ?><br>
+							<small>(<?php echo lang('profile_picture_guidelines'); ?>)</small>
+						</p>
+					</div>
+				</div>
 
-						<div class="accountPicSelect clearfix">
-							<div class="pull-left">
-								<input type="radio" name="pic_selection" value="gravatar" />
-								<?php echo showPhoto( $gravatar ); ?>
-							</div>
-							<div class="pull-left">
-								<p>
-									<small><a href="http://gravatar.com/" target="_blank">Gravatar</a></small>
-								</p>
-							</div>
-						</div>
-					
-					<?php endif; ?>
-                    </p>
-					<?php if ( ! isset($account_details->picture)) : ?>
-					<?php endif; ?>
-
-					<?php if (isset($profile_picture_error))
-				{
-					?>
-                    <span class="help-inline">
-					<?php echo $profile_picture_error; ?>
-					</span>
-					<?php } ?>
+				<div class="accountPicSelect clearfix">
+					<div class="pull-left">
+						<input type="radio" name="pic_selection" value="gravatar" />
+						<?php echo showPhoto( $gravatar ); ?>
+					</div>
+					<div class="pull-left">
+						<p>
+							<small><a href="http://gravatar.com/" target="_blank"><?php echo lang('profile_gravatar'); ?></a></small>
+						</p>
+					</div>
+				</div>
+			
+			<?php endif; ?>
+    </p>
+			<?php if (isset($profile_picture_error))
+			{
+			?>
+			<span class="help-inline">
+				<?php echo $profile_picture_error; ?>
+			</span>
+			<?php } ?>
                 </div>
             </div>
 
-            <div class="form-actions">
-                <button type="submit" class="btn btn-primary"><?php echo lang('profile_save'); ?></button>
-            </div>
-
-			<?php echo form_fieldset_close(); ?>
-			<?php echo form_close(); ?>
-
+		<div class="form-actions">
+		    <button type="submit" class="btn btn-primary"><?php echo lang('profile_save'); ?></button>
+		</div>
+		<?php echo form_fieldset_close(); ?>
+		<?php echo form_close(); ?>
         </div>
     </div>
 </div>
